086ecb1698497f40c169bfc8969550bdd5b542cf
Anton Kutsim <kutsimanton@gmail.com>
2022-08-24 23:04:42 +0300

    add/propTypes

--- src/components/ContactsList/ContactsList.js
+++ src/components/ContactsList/ContactsList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from 'prop-types';
 const ContactsList = ({ contacts, onDelContact }) => {
     return (
         <>
@@ -15,5 +16,8 @@ const ContactsList = ({ contacts, onDelContact }) => {
         </>
     )
 }
-
+ContactsList.propTypes = {
+    contacts: PropTypes.array.isRequired,
+    onDelContact: PropTypes.func.isRequired
+}
 export default ContactsList;
